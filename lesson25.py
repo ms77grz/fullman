c22e654b26cb577ff404d043742c01a1ab642482
ms77grz <ms77grz@gmail.com>
2019-11-12 13:56:46 +0300

    changes

--- lesson25.py
+++ lesson25.py
@@ -4,17 +4,17 @@ guesses = 0
 print('Угадайте число от 1 до 100')
 while True:
     guess = int(input('> '))
-
     guesses += 1
-    if guesses == 3:
-        print('Вы не угадали')
-        break
-    elif guess == secret:
+    if guess == secret:
         print('Вы угадали число!')
         break
+    elif guesses == 3:
+        print('Вы не угадали')
+        break
     else:
-        print('Попробуйте еще раз')
+        if guess > secret:
+            print('Загаданное число меньше.\nПопробуйте еще раз')
+        else:
+            print('Загаданное число больше.\nПопробуйте еще раз')
 else:
     print('Вы угадали число! Поздравляем!')
-
-    print('Попробуйте еще раз')
