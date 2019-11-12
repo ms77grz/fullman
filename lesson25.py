c9385d4a3e92a19bff0a1d0aec49711a4147142e
ms77grz <ms77grz@gmail.com>
2019-11-12 11:38:00 +0300

    some changes

--- lesson25.py
+++ lesson25.py
@@ -1,11 +1,11 @@
-secret = 25
+secret = 35
 guess = None
 guesses = 0
 print('Угадайте число от 1 до 100')
 while True:
     guess = int(input('> '))
-    
-    guesses +=1
+
+    guesses += 1
     if guesses == 3:
         print('Вы не угадали')
         break
