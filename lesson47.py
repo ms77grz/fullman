02c74ec28cedd41b78c5faeed41803b1d9c6233d
ms77grz <ms77grz@gmail.com>
2019-11-28 21:19:22 +0300

    smth

--- lesson47.py
+++ lesson47.py
@@ -6,7 +6,41 @@ cursor = db.cursor()
 
 email = 't.cruise@gmail.com'
 cursor.execute('SELECT * FROM users WHERE email=?', (email,))
-result = cursor.fetchone()
-print(result)
+print(cursor.fetchone())
+
+email = 'rocklee@gmail.com'
+cursor.execute('SELECT * FROM users WHERE email = ? OR name = ?',
+               (email, 'Rock Lee'))
+print(cursor.fetchone())
+
+cursor.execute('SELECT * FROM users WHERE email = :email OR name = :name',
+               {'email': email, 'name': 'Sarah Conor'})
+print(cursor.fetchone())
+
+cursor.execute('SELECT * FROM users')
+result = cursor.fetchall()
+for user in result:
+    print(user)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+db.row_factory = dict_factory
+cursor = db.cursor()
+
+cursor.execute('SELECT * FROM users')
+result = cursor.fetchall()
+for user in result:
+    print(user['name'], user['email'])
+
+cursor.execute('INSERT INTO users (name, email) VALUES ("User 4", "user4@gmail.com")')
+cursor.execute('INSERT INTO users (name, email) VALUES ("User 5", "user5@gmail.com")')
+db.commit()
+print(db.total_changes)
 
 db.close()
